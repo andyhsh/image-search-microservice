b80902142d028866a931326690aff205c3fca652
Andy Ho <89andy@gmail.com>
2018-04-03 15:13:16 +0800

    Add pagination for api

--- controllers/imageSearch.js
+++ controllers/imageSearch.js
@@ -6,9 +6,10 @@ require('dotenv').config();
 
 const fetchImages = (req, res) => {
   const path = 'https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=';
-
+  const count = 10;
+  const offset = req.query.offset * 10 || 0;
   superAgent
-    .get(path + req.params.id)
+    .get(`${path}${req.params.id}&count=${count}&offset=${offset}`)
     .set('Ocp-Apim-Subscription-Key', process.env.AZURE_API_KEY)
     .then(imagesJson => {
       res.send(JSON.parse(imagesJson.text));
